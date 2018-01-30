2dfe7bd44dd8d712e89d6a115d174886f92dcad9
NMSeifert <nicholas.m.seifert@gmail.com>
2018-01-29 23:36:31 -0500

    Added a very basic card class

---
+++ Card Game/card.hpp
@@ -0,0 +1,10 @@
+#ifndef CARD_HPP
+#define CARD_HPP
+
+class card {
+	public: 
+	int suit;
+	int rank;
+};
+
+#endif
--- Card Game/war.cpp
+++ Card Game/war.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include "card.hpp"
  
 int main(int argc, char *argv[]){
    std::cout << "Hello World!" << std::endl;
