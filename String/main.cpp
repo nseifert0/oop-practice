b24cdca33ea72c28d02bd0d2b7790674ae20d312
NMSeifert <nicholas.m.seifert@gmail.com>
2018-02-06 19:35:08 -0500

    Added a file for creating a string class

---
+++ String/main.cpp
@@ -0,0 +1,28 @@
+#include "string.hpp"
+
+int main() {
+	// Default construct
+	String s = "Test";
+	string s0;
+
+	// Construct and assign from a literal value
+	string s1 = "hello";
+	s1 = "foo";
+
+	// Copy construct and assign
+	string s2 = s1;
+	s2 = s1; // s2 == s1
+
+	// Move construction and assignment (optional)
+	string s3 = std::move(s2);
+	s3 = std::move(s1);
+
+	// Compare
+	s1 == s1;
+
+	// Order
+	s1 < s1; // lexicographical comparison
+
+	// Object independence
+	s2 = "bar"; // Does not change the value of s1
+}
