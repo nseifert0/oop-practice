9431e12545b1da8b21b039e55266bd63a6aeb845
NMSeifert <nicholas.m.seifert@gmail.com>
2018-02-08 09:09:26 -0500

    Added some better comparison operations at the last minute.

--- String/string.cpp
+++ String/string.cpp
@@ -1,15 +1,43 @@
 #include "string.hpp"
 
 bool string::operator==(const string& str) {
-	if(this->length == str.length)
-		return true;
-	else
+	if(this->length != str.length) {
 		return false;
+	}
+	else {
+		for(int i=0; i< this->length; i++) {
+			if(this->s[i] != str.s[i]) {
+				return false;
+			}
+		}
+	}
+	
+	return true;
 }
 
 bool string::operator<(const string& str) {
-	if(this->length < str.length)
-		return true;
-	else
-		return false;
+	
+	if(this->length < str.length) {
+		for(int i=0; i< this->length; i++) {
+			if(this->s[i] < str.s[i]) {
+				return true;
+			}
+			else if(this->s[i] > str.s[i]) {
+				return false;
+			}
+		}
+	}
+	
+	if(this->length > str.length) {
+		for(int i=0; i< str.length; i++) {
+			if(this->s[i] < str.s[i]) {
+				return true;
+			}
+			else if(this->s[i] > str.s[i]) {
+				return false;
+			}
+		}
+	}
+	
+	return false;
 }
