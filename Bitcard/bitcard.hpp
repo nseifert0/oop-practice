f5ab8801011af086d60d3a535703a0ab0b44b3a8
NMSeifert <nicholas.m.seifert@gmail.com>
2018-02-15 07:48:30 -0500

    Added bitcard class.

--- Bitcard/bitcard.hpp
+++ Bitcard/bitcard.hpp
@@ -1,4 +1,55 @@
 #ifndef BITCARD_HPP
 #define BITCARD_HPP
 
+enum Rank {
+  Ace,
+  Two,
+  Three,
+  Four,
+  Five,
+  Six,
+  Seven,
+  Eight,
+  Nine,
+  Ten,
+  Jack,
+  Queen,
+  King,
+};
+
+enum Suit {
+  Hearts,
+  Diamonds,
+  Clubs, 
+  Spades,
+};
+
+class Bitcard {
+public:
+
+  Bitcard(Rank r, Suit s)
+    : bits((unsigned)s << 4 | (unsigned)r)
+  { }
+
+  Rank get_rank() const {
+    return (Rank)(0b001111 & bits);
+  }
+
+  Suit get_suit() const {
+    return (Suit)((0b110000 & bits) >> 4);
+  }
+
+  bool operator==(Bitcard c) const {
+    return bits == c.bits;
+  }
+
+  bool operator!=(Bitcard c) const {
+    return bits != c.bits;
+  }
+
+private:
+  unsigned char bits;
+};
+
+
 #endif
