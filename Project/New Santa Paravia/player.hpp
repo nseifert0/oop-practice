c14f117bc092c1956c6593b5997caf9163d8c451
NMSeifert <nicholas.m.seifert@gmail.com>
2018-03-13 09:55:44 -0400

    Added simple constructor and some member variables to Player class

--- Project/New Santa Paravia/player.hpp
+++ Project/New Santa Paravia/player.hpp
@@ -1,8 +1,22 @@
 #ifndef PLAYER_HPP
 #define PLAYER_HPP
+#include <string>
+
+enum Title {
+	Sir,
+	Count
+};
 
 class Player {
-	
+	public:
+		Player(std::string n, Title t)
+			:name(n), title(t)
+		{
+		}
+		
+	private:
+		std::string name;
+		Title title;
 };
 
 #endif
