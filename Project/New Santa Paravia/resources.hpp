b512d0e6133ccbd0683cd88fc6f7a0e9c9d2c562
NMSeifert <nicholas.m.seifert@gmail.com>
2018-03-15 10:28:12 -0400

    Resources object now has default values of all resources.

--- Project/New Santa Paravia/resources.hpp
+++ Project/New Santa Paravia/resources.hpp
@@ -14,19 +14,20 @@ class Resources {
 		int customsDuty = 25;
 		int grainPrice = 25;
 		int grainReserve = 5000;
+		int incomeTax = 5;
 		int justice = 2;
 		int land = 10000;
+		double landPrice = 10.0;
 		int marketplaces = 0;
 		int merchants = 25;
 		int mills = 0;
 		int nobles = 4;
 		int palace = 0;
-		double publicWorks = 1.0;
-		double landPrice = 10.0;
-		int customsDutyRevenue;
-		int deadSerfs;
+		double publicWorks = 1.0;		
+		int salesTax = 10;
+		int serfs = 2000;
+		int soldiers = 25;
 		int treasury = 1000;
-		int serfs;
 };
 
 #endif
