734805c0d82a7f0ca6baf4aaa0a112a3c8b524f0
NMSeifert <nicholas.m.seifert@gmail.com>
2018-04-12 12:14:21 -0400

    Implemented print function better

--- JSON/json.hpp
+++ JSON/json.hpp
@@ -15,29 +15,43 @@ class Value {
 		{
 
 		}
+		virtual void print() const = 0;
 		virtual ~json() = default;
 	private:
 
 };
 
 class Null : Value {
-	
+	void print() const override {
+		std::cout << "\n";
+	}
 };
 
 class Bool : Value {
 	bool val;
+	void print() const override {
+		std::cout << val << "\n";
+	}
 };
 
 class Number : Value {
 	double val;
+	void print() const override {
+		std::cout << val << "\n";
+	}
 };
 
 class String : Value {
 	string val;
+	void print() const override {
+		std::cout << val << "\n";
+	}
 };
 
 class Array : Value , vector<value *>{
-	
+	void print() const override {
+		std::cout << val << "\n";
+	}
 };
 
 class Object : Value {
