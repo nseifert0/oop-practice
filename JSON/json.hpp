6d7e53bf707ec2dc5c65f4a74675aeb3d1abfddc
NMSeifert <nicholas.m.seifert@gmail.com>
2018-04-12 08:44:21 -0400

    Added JSON stuff

---
+++ JSON/json.hpp
@@ -0,0 +1,20 @@
+//Nicholas Seifert
+
+#ifndef JSON_HPP
+#define JSON_HPP
+
+#include <cstring>
+
+class json {
+	public:
+		json()
+		{
+
+		}
+		
+	private:
+		int length;
+		char* s;
+};
+
+#endif
---
+++ JSON/main.cpp
@@ -0,0 +1,6 @@
+#include "json.hpp"
+#include <iostream>
+
+int main() {
+	std::cout << "Hello World \n";
+}
